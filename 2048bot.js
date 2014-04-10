681b5928217d5746fcb9d7b42e851b4c19bc0942
triestpa <triestpa@grinnell.edu>
2014-04-10 04:45:01 -0500

    add first bot

---
+++ 2048bot.js
@@ -0,0 +1,36 @@
+
+a = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+b = a.addRandomTile.bind(a);
+moved = false;
+a.addRandomTile = function() { moved = true; b(); };
+movesfailed = 0;
+
+function checkMove() {
+   if (moved == false) {
+    movesfailed++;
+  }
+  else {
+    movesfailed = 0;
+  }
+}
+
+function stackTopRight(move) {
+  a.move(move);
+  return moved;
+}
+
+setInterval(function() {
+  moved = false;  
+
+if (a.isGameTerminated()) {
+    window.setTimeout(function() {
+      a.restart();
+    }
+      ,2000)
+  }
+  else if(stackTopRight(1)) {}
+  else if (stackTopRight(0)) {}
+  else if (stackTopRight(3)) {}
+  else if (stackTopRight(2)) {}
+}, 250);
+
